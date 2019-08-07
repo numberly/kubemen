e421d0df46ba1188d1048aa12c10f1d9c0307e8f
Vincent Trubesset <vincent.trubesset@numberly.com>
2019-08-07 10:18:24 +0200

    Apply a different style for reload operations while reworking the message customisation flow

--- kubemen/connectors.py
+++ kubemen/connectors.py
@@ -4,9 +4,10 @@ import random
 import requests
 from flask import current_app
 
-ATTACHMENT_STYLE = {
+MESSAGE_STYLE = {
     "CREATE": {"color": "#228b22", "emoji": ":rocket:"},
-    "UPDATE": {"color": "#1e90ff", "emoji": ":recycle:"},
+    "UPDATE": {"color": "#1e90ff", "emoji": ":zap:"},
+    "RELOAD": {"color": "#ff8c00", "emoji": ":recycle:"},
     "DELETE": {"color": "#dc143c", "emoji": ":x:"}
 }
 WATCHMEN_MEMBERS = ["Doctor Manhattan", "Nite Owl", "Ozymandias",
@@ -15,34 +16,34 @@ WATCHMEN_MEMBERS = ["Doctor Manhattan", "Nite Owl", "Ozymandias",
 
 def send_mattermost_message(operation, hashtag, namespace, kind, name,
                             username, images, diff, fancyness_level):
-    color = ATTACHMENT_STYLE[operation]["color"]
-    emoji = ATTACHMENT_STYLE[operation]["emoji"]
-    text = current_app.config.get("MATTERMOST_TEXT_MESSAGE_FORMAT")
-    text = text.format(emoji=emoji, hashtag=hashtag, namespace=namespace,
-                       kind=kind, name=name, operation=operation.lower(),
-                       username=username)
-
-    attachment = {"color": color, "fields": []}
+    fields = []
     if images:
         value = ""
         for image in images:
             value += "- `{}`\n".format(image)
-        attachment["fields"].append({"short": False,
-                                     "title": "Images",
-                                     "value": value})
+        fields.append({"title": "Images", "value": value})
     if diff:
-        attachment["fields"].append({"short": False,
-                                     "title": "YAML configuration diff",
-                                     "value": "```diff\n{}```".format(diff)})
+        fields.append({"title": "YAML configuration diff",
+                       "value": "```diff\n{}```".format(diff)})
     elif kind == "Deployment" and operation == "UPDATE":
-        text = text.replace("updated", "reloaded")
+        operation = "RELOAD"
+
+    emoji = MESSAGE_STYLE[operation]["emoji"]
+    text = current_app.config.get("MATTERMOST_TEXT_MESSAGE_FORMAT")
+    text = text.format(emoji=emoji, hashtag=hashtag, namespace=namespace,
+                       kind=kind, name=name, operation=operation,
+                       username=username)
 
     # TODO: retrieve channel_id from annotation
-    message = {"channel_id": "bot"}
+    message = {"channel_id": "bot", "text": text}
+    if fields:
+        color = MESSAGE_STYLE[operation]["color"]
+        attachment = {"color": color, "fields": fields}
+        message.update({"attachments": [attachment]})
     icons_base_url = current_app.config.get("ICONS_BASE_URL")
-    if fancyness_level > 0:
+    if fancyness_level > 0 and fields:
         thumb_url = icons_base_url.format("kubemen")
-        attachment.update(thumb_url=thumb_url)
+        message["attachments"][0].update(thumb_url=thumb_url)
     if fancyness_level > 1:  # TODO: test fancyness
         # Randomly select a Watchmen member as notifier
         bot_username = random.choice(WATCHMEN_MEMBERS)
@@ -50,8 +51,5 @@ def send_mattermost_message(operation, hashtag, namespace, kind, name,
         icon_url = icons_base_url.format(icon_filename)
         message.update(username=bot_username, icon_url=icon_url)
 
-    message.update(text=text)
-    if attachment["fields"]:
-        message.update({"attachments": [attachment]})
     mattermost_hook_url = current_app.config.get("MATTERMOST_HOOK_URL")
     requests.post(mattermost_hook_url, data=json.dumps(message))
