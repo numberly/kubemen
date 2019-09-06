80cdae96c0455f9ac653c149447bacb54743989e
Guillaume Gelin <ramnes@numberly.com>
2019-09-06 19:26:41 +0200

    Make tests simpler with a review fixture

--- tests/app.py
+++ tests/app.py
@@ -6,80 +6,33 @@ def test_get(client):
     assert response.status_code == 200
 
 
-def test_post_skip_alerting_service_account(client):
-    payload = {
-        "kind": "AdmissionReview",
-        "request": {
-            "namespace": "magrathea",
-            "object": {
-                "kind": "PersistentVolumeClaims",
-                "metadata": {
-                    "name": "the-answer-to-the-ultimate-question"
-                }
-            },
-            "operation": "CREATE",
-            "uid": "121593ce-bb88-4c69-1fa2-c4ea619cfa4c",
-            "userInfo": {"username": "serviceaccount:slartibartfast"}
-        }
-    }
-    response = client.post("/", data=json.dumps(payload))
+def test_post_skip_alerting_service_account(client, review):
+    review["request"]["userInfo"]["username"] = "serviceaccount:foo"
+    response = client.post("/", data=json.dumps(review))
     assert response.status_code == 200
     data = response.get_json()
-    assert data["response"]["uid"] == payload["request"]["uid"]
+    assert data["response"]["uid"] == review["request"]["uid"]
     assert data["response"]["allowed"] is True
 
 
-def test_post_skip_alerting_no_domain(client):
-    payload = {
-        "kind": "AdmissionReview",
-        "request": {
-            "namespace": "magrathea",
-            "object": {
-                "kind": "PersistentVolumeClaims",
-                "metadata": {
-                    "name": "the-answer-to-the-ultimate-question"
-                }
-            },
-            "operation": "CREATE",
-            "uid": "121593ce-bb88-4c69-1fa2-c4ea619cfa4c",
-            "userInfo": {"username": "deep_thought"}
-        }
-    }
-    response = client.post("/", data=json.dumps(payload))
+def test_post_skip_alerting_no_domain(client, review):
+    review["request"]["userInfo"]["username"] = "deep_thought"
+    response = client.post("/", data=json.dumps(review))
     assert response.status_code == 200
     data = response.get_json()
-    assert data["response"]["uid"] == payload["request"]["uid"]
+    assert data["response"]["uid"] == review["request"]["uid"]
     assert data["response"]["allowed"] is True
 
 
 # TODO: test message content
 # TODO: test images in message field
-def test_post_create_deployment(client):
-    payload = {
-        "kind": "AdmissionReview",
-        "request": {
-            "namespace": "magrathea",
-            "object": {
-                "kind": "Deployment",
-                "metadata": {
-                    "name": "the-answer-to-the-ultimate-question"
-                }
-            },
-            "oldObject": {
-                "kind": "PersistentVolumeClaims",
-                "metadata": {
-                    "name": "the-answer-to-the-ultimate-question"
-                }
-            },
-            "operation": "CREATE",
-            "uid": "121593ce-bb88-4c69-1fa2-c4ea619cfa4c",
-            "userInfo": {"username": "deep_thought@numberly.com"}
-        }
-    }
-    response = client.post("/", data=json.dumps(payload))
+def test_post_create_deployment(client, review):
+    review["request"]["operation"] = "CREATE"
+    del review["request"]["oldObject"]
+    response = client.post("/", data=json.dumps(review))
     assert response.status_code == 200
     data = response.get_json()
-    assert data["response"]["uid"] == payload["request"]["uid"]
+    assert data["response"]["uid"] == review["request"]["uid"]
     assert data["response"]["allowed"] is True
     # requests.post.assert_called_once_with(data={"channel_id": "bot"})
 
@@ -87,90 +40,31 @@ def test_post_create_deployment(client):
 # TODO: test message content
 # TODO: test diff in message field
 # TODO: test diff corner cases (useless paths + exceptions)
-def test_post_update(client):
-    payload = {
-        "kind": "AdmissionReview",
-        "request": {
-            "namespace": "magrathea",
-            "object": {
-                "kind": "Deployment",
-                "metadata": {
-                    "name": "the-answer-to-the-ultimate-question"
-                },
-                "spec": {
-                    "template": {"spec": {"containers": [{"image": "6.9"}]}}
-                }
-            },
-            "oldObject": {
-                "kind": "Deployment",
-                "metadata": {
-                    "name": "the-answer-to-the-ultimate-question"
-                },
-                "spec": {
-                    "template": {"spec": {"containers": [{"image": "4.2"}]}}
-                }
-            },
-            "operation": "UPDATE",
-            "uid": "121593ce-bb88-4c69-1fa2-c4ea619cfa4c",
-            "userInfo": {"username": "deep_thought@numberly.com"}
-        }
-    }
-    response = client.post("/", data=json.dumps(payload))
+def test_post_update(client, review):
+    response = client.post("/", data=json.dumps(review))
     assert response.status_code == 200
     data = response.get_json()
-    assert data["response"]["uid"] == payload["request"]["uid"]
+    assert data["response"]["uid"] == review["request"]["uid"]
     assert data["response"]["allowed"] is True
 
 
 # TODO: test message content
-def test_post_reloaded(client):
-    payload = {
-        "kind": "AdmissionReview",
-        "request": {
-            "namespace": "magrathea",
-            "object": {
-                "kind": "Deployment",
-                "metadata": {
-                    "name": "the-answer-to-the-ultimate-question"
-                }
-            },
-            "oldObject": {
-                "kind": "Deployment",
-                "metadata": {
-                    "name": "the-answer-to-the-ultimate-question"
-                }
-            },
-            "operation": "UPDATE",
-            "uid": "121593ce-bb88-4c69-1fa2-c4ea619cfa4c",
-            "userInfo": {"username": "deep_thought@numberly.com"}
-        }
-    }
-    response = client.post("/", data=json.dumps(payload))
+def test_post_reloaded(client, review):
+    del review["request"]["oldObject"]["spec"]
+    del review["request"]["object"]["spec"]
+    response = client.post("/", data=json.dumps(review))
     assert response.status_code == 200
     data = response.get_json()
-    assert data["response"]["uid"] == payload["request"]["uid"]
+    assert data["response"]["uid"] == review["request"]["uid"]
     assert data["response"]["allowed"] is True
 
 
 # TODO: test message content
-def test_post_delete(client):
-    payload = {
-        "kind": "AdmissionReview",
-        "request": {
-            "namespace": "magrathea",
-            "oldObject": {
-                "kind": "PersistentVolumeClaims",
-                "metadata": {
-                    "name": "the-answer-to-the-ultimate-question"
-                }
-            },
-            "operation": "DELETE",
-            "uid": "121593ce-bb88-4c69-1fa2-c4ea619cfa4c",
-            "userInfo": {"username": "deep_thought@numberly.com"}
-        }
-    }
-    response = client.post("/", data=json.dumps(payload))
+def test_post_delete(client, review):
+    review["request"]["operation"] = "DELETE"
+    del review["request"]["object"]
+    response = client.post("/", data=json.dumps(review))
     assert response.status_code == 200
     data = response.get_json()
-    assert data["response"]["uid"] == payload["request"]["uid"]
+    assert data["response"]["uid"] == review["request"]["uid"]
     assert data["response"]["allowed"] is True
--- tests/conftest.py
+++ tests/conftest.py
@@ -14,3 +14,34 @@ def client():
 @pytest.fixture(autouse=True)
 def mattermock(mocker):
     mocker.patch.object(requests, "post")
+
+
+@pytest.fixture
+def review():
+    return {
+        "kind": "AdmissionReview",
+        "request": {
+            "namespace": "magrathea",
+            "object": {
+                "kind": "Deployment",
+                "metadata": {
+                    "name": "the-answer-to-the-ultimate-question"
+                },
+                "spec": {
+                    "template": {"spec": {"containers": [{"image": "6.9"}]}}
+                }
+            },
+            "oldObject": {
+                "kind": "Deployment",
+                "metadata": {
+                    "name": "the-answer-to-the-ultimate-question"
+                },
+                "spec": {
+                    "template": {"spec": {"containers": [{"image": "4.2"}]}}
+                }
+            },
+            "operation": "UPDATE",
+            "uid": "121593ce-bb88-4c69-1fa2-c4ea619cfa4c",
+            "userInfo": {"username": "deep_thought@numberly.com"}
+        }
+    }
